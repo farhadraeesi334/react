497c3e6f68e1f2f40c7c9cad1445084c4c5e2afe
Ali <aliikm1389@gmail.com>
2025-05-23 11:44:11 +0330

    Update Menu.jsx

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,1 +1,20 @@
-
+export default function Menu(){
+return(
+<>
+ <nav class="navbar">
+    <div class="logo">سایت من</div>
+    <div class="nav-links" id="navLinks">
+      <a href="#">خانه</a>
+      <a href="#">درباره ما</a>
+      <a href="#">خدمات</a>
+      <a href="#">تماس</a>
+    </div>
+    <div class="hamburger" onclick="toggleMenu()">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </nav>
+</>
+)
+}
